fc043a29cbb2e70f343586490b73768de770a53a
Celsheaamelia <celsheeamelialailaputri@gmail.com>
2026-09-05 12:58:00 +0700

    perubahan

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'role' => \App\Http\Middleware\EnsureUserRole::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+
